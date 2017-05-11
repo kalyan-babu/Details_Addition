dc6d9e2aa87707864850b2d2bbbe5cd80e3fa5d9
kalyan <kalyanbuddavarapu@gmail.com>
2017-05-11 10:51:04 +0530

    File to show error in Session.

---
+++ session_error.php
@@ -0,0 +1,11 @@
+<?php
+
+echo'<br><br><br><br><div class="container">
+		<div align="center">
+		<p>Soryy, NO Session has created.</p>
+		<p>Please, <a href="main_page.php">click_here</a> to go to HOME PAGE</p>
+		<p>START new SESSION</p>
+		</div></div>';
+		
+	
+?>
